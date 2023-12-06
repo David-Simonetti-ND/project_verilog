eff7536f5f3bb9c1aa8ee6c2db2d799d4423168e
David-Simonetti-ND <dsimone2@nd.edu>
2023-12-06 12:44:36 -0500

    skeleton for project laid out

---
+++ datapath.v
@@ -0,0 +1,12 @@
+module encryption
+(
+  input wire [7:0] data ,
+  input wire [7:0] key  ,
+  input wire clk,
+  input wire rst,
+  output reg [7:0] encrypted_out
+);
+
+    /* something something perform the AES encryption here */
+
+endmodule
