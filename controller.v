b61a661e475d8149cae550c634366569dd705376
David-Simonetti-ND <dsimone2@nd.edu>
2023-12-06 15:20:15 -0500

    Datapath done

--- controller.v
+++ controller.v
@@ -1,44 +1,51 @@
 module controller
 (
-  input wire [7:0] data ,
-  input wire [7:0] key  ,
-  /* output signals to datapath go here
-  output reg        sel    , // select instruction address to memory
-  output reg        rd     , // enable memory output onto data bus
-  output reg        ld_ir  , // load instruction register
-  output reg        inc_pc , // increment program counter
-  output reg        halt   , // halt machine
-  output reg        ld_pc  , // load program counter
-  output reg        data_e , // enable accumulator output onto data bus
-  output reg        ld_ac  , // load accumulator from data bus
-  output reg        wr       // write data bus to memory
-  */
+  //input wire [7:0] data      ,
+  input wire input_data_ready      ,
+  input wire is_multiplication_done,
+  output reg initialize            ,
+  output reg en_multiply           ,
+  output reg en_modulo             ,
+  output reg done
 );
 
-    /*
-    FSM states
-    localparam integer HLT=0, SKZ=1, ADD=2, AND=3, XOR=4, LDA=5, STO=6, JMP=7;
-    */
+    // FSM states
+    localparam integer WAITING=0, INITIALIZE=1, MULTIPLY=2, MODULO=3, DONE=4;
 
-    /*
-    logic to assign outputs based on inputs
+    reg [2:0] current_state = WAITING;
+    reg [2:0] next_state;
+
     always @* begin
-      H = (opcode == HLT);
-      A = (opcode == ADD || opcode == AND || opcode == XOR || opcode == LDA);
-      Z = (opcode == SKZ && zero);
-      J = (opcode == JMP);
-      S = (opcode == STO);
-      case ( phase )
-        0:begin sel=1;rd=0;ld_ir=0;inc_pc=0;halt=0;ld_pc=0;data_e=0;ld_ac=0;wr=0;end
-        1:begin sel=1;rd=1;ld_ir=0;inc_pc=0;halt=0;ld_pc=0;data_e=0;ld_ac=0;wr=0;end
-        2:begin sel=1;rd=1;ld_ir=1;inc_pc=0;halt=0;ld_pc=0;data_e=0;ld_ac=0;wr=0;end
-        3:begin sel=1;rd=1;ld_ir=1;inc_pc=0;halt=0;ld_pc=0;data_e=0;ld_ac=0;wr=0;end
-        4:begin sel=0;rd=0;ld_ir=0;inc_pc=1;halt=H;ld_pc=0;data_e=0;ld_ac=0;wr=0;end
-        5:begin sel=0;rd=A;ld_ir=0;inc_pc=0;halt=0;ld_pc=0;data_e=0;ld_ac=0;wr=0;end
-        6:begin sel=0;rd=A;ld_ir=0;inc_pc=Z;halt=0;ld_pc=J;data_e=S;ld_ac=0;wr=0;end
-        7:begin sel=0;rd=A;ld_ir=0;inc_pc=0;halt=0;ld_pc=J;data_e=S;ld_ac=A;wr=S;end
-      endcase
+        initialize = 0;
+        en_multiply = 0;
+        en_modulo = 0;
+        done = 0;
+        case(current_state)
+            WAITING:
+                if (input_data_ready) {
+                    next_state = INITIALIZE;
+                }
+                else {
+                    next_state = WAITING;
+                }
+            INITIALIZE:
+                initialize = 1;
+                next_state = MULTIPLY;
+            MULTIPLY:
+                if (is_multiplication_done) {
+                    next_state = DONE;
+                }
+                else {
+                    en_multiply = 1;
+                    next_state = MODULO;
+                }
+            MODULO:
+                en_modulo = 1;
+                next_state = MULTIPLY;
+            DONE:
+                done = 1;
+                next_state = INITIALIZE;
+        endcase
+        current_state = next_state;
     end
-    */
-
 endmodule
